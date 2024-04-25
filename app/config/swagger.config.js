7e52ae22e6cea9f312933793281f227e9edbb186
laainra <lailaar77@gmail.com>
2024-04-25 14:24:16 +0700

    add new server url

--- app/config/swagger.config.js
+++ app/config/swagger.config.js
@@ -1,36 +1,35 @@
 module.exports = {
-    definition: {
-      openapi: "3.1.0",
-      info: {
-        title: "SANS AGV API",
-        version: "0.1.0",
-        description:
-          "AGV interface",
-        license: {
-          name: "MIT",
-          url: "https://spdx.org/licenses/MIT.html",
-        },
+  definition: {
+    openapi: "3.1.0",
+    info: {
+      title: "SANS AGV API",
+      version: "0.1.0",
+      description: "AGV interface",
+      license: {
+        name: "MIT",
+        url: "https://spdx.org/licenses/MIT.html",
       },
-      components: {
-        securitySchemes: {
-          Bearer: {
-            type: 'http',
-            scheme: 'bearer',
-            bearerFormat: 'JWT',
-          }
-        }
-      },
-      servers: [
-        {
-          url: "https://sans-api-service.onrender.com/api/",
-        },
-        {
-          url: "http://localhost:9000/api/",
-        },
-        {
-          url: "https://sans-agv.azurewebsites.net/api/",
-        },
-      ],
     },
-    apis: ["./app/routes/*.js", "./app/models/*.js","./app/ws/*.js",],
-  };
+    components: {
+      securitySchemes: {
+        Bearer: {
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT",
+        },
+      },
+    },
+    servers: [
+      {
+        url: "https://sans-api-service.onrender.com/api/",
+      },
+      {
+        url: "http://localhost:9000/api/",
+      },
+      {
+        url: "https://sans-api-service-bde60fbdfff2.herokuapp.com/api/",
+      },
+    ],
+  },
+  apis: ["./app/routes/*.js", "./app/models/*.js", "./app/ws/*.js"],
+};
